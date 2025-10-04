b982788d8334b17d0f94ffb9a60545cd0fe8bcf0
Agustin Choque <agustinchoque.dev@gmail.com>
2025-10-04 19:27:31 -0300

    Modulo 3: Control de flujo

--- src/Main.java
+++ src/Main.java
@@ -4,28 +4,34 @@ public class Main {
     public static void main(String[] args) {
         int prioridad_total = 0;
         Scanner scanner = new Scanner(System.in);
+        int opcion;
 
-        System.out.println("=== Gestor de tareas ===");
-        System.out.println("1. Agregar tarea");
-        System.out.println("2. Ver tareas (pendiente)");
-        System.out.println("3. Salir");
+        do {
+            System.out.println("=== Gestor de tareas ===");
+            System.out.println("1. Agregar tarea");
+            System.out.println("2. Ver tareas (pendiente)");
+            System.out.println("3. Salir");
 
-        int opcion = scanner.nextInt();
-        scanner.nextLine();
-
-        if(opcion >= 1 && opcion <= 3) {
+            opcion = scanner.nextInt();
+            scanner.nextLine();
             switch (opcion) {
                 case 1:
                     System.out.println("Ingrese el nombre de la tarea:");
                     String nombre_tarea = scanner.nextLine();
-                    System.out.println("Ingrese la prioridad de la tarea (1 a 5):");
-                    int prioridad = scanner.nextInt();
-                    scanner.nextLine();
-                    if(prioridad < 1 || prioridad > 5) {
-                        System.out.println("La prioridad debe ser un numero entero entre el 1 y el 5.");
-                        break;
-                    }
+                    int prioridad;
+
+                    do {
+                        System.out.println("Ingrese la prioridad de la tarea (1 a 5):");
+                        prioridad = scanner.nextInt();
+                        scanner.nextLine();
+
+                        if (prioridad < 1 || prioridad > 5) {
+                            System.out.println("La prioridad debe ser un numero entero entre el 1 y el 5.");
+                        }
+                    } while (prioridad < 1 || prioridad > 5);
+
                     prioridad_total += prioridad;
+
                     System.out.println("Tarea creada: " + nombre_tarea + "(prioridad: " +  prioridad + ")");
                     System.out.println("Suma total de prioridades: " + prioridad_total);
                     break;
@@ -35,10 +41,18 @@ public class Main {
                 case  3:
                     System.out.println("¡Nos vemos!");
                     break;
+                default:
+                    System.out.println("Opcion invalida. Vuelve a intentarlo.");
             }
-        } else {
-            System.out.println("Opcion invalida.");
-        }
+        } while (opcion != 3);
+
         scanner.close();
+
+
+
+
+
+
+
     }
 }
